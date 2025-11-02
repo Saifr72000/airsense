db2b013dc5fd84e10685a76417c815ed892dec00
Saifr72000 <saif.rana@me.com>
2025-11-02 17:16:27 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+let tempDS = 0
+let humDHT = 0
+let tempDHT = 0
+OLED.init(128, 64)
+ESP8266_IoT.connectWifi("Telenor5216sal", "your_pwd")
+ESP8266_IoT.setMQTT(
+ESP8266_IoT.SchemeList.TCP,
+"",
+"",
+"",
+""
+)
+ESP8266_IoT.connectMQTT("10.0.0.3", 1883, true)
+basic.forever(function () {
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P2,
+    true,
+    false,
+    true
+    )
+    tempDHT = dht11_dht22.readData(dataType.temperature)
+    humDHT = dht11_dht22.readData(dataType.humidity)
+    tempDS = Environment.Ds18b20Temp(DigitalPin.P1, Environment.ValType.DS18B20_temperature_C)
+    OLED.clear()
+    OLED.writeStringNewLine("DS Temp: " + convertToText(tempDS) + " C")
+    OLED.writeStringNewLine("DHT Temp: " + convertToText(tempDHT) + " C")
+    OLED.writeStringNewLine("DHT Hum: " + convertToText(humDHT) + " %")
+    serial.writeLine("TempDS: " + convertToText(tempDS) + " C")
+    serial.writeLine("TempDHT: " + convertToText(tempDHT) + " C")
+    serial.writeLine("Hum: " + convertToText(humDHT))
+    if (ESP8266_IoT.isMqttBrokerConnected()) {
+    	
+    }
+})
