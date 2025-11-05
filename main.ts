94879471107f457603d253738f934e94efe22834
Saifr72000 <saif.rana@me.com>
2025-11-05 20:42:19 +0000

    wifi connection fixed

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let tempDS = 0
 let humDHT = 0
 let tempDHT = 0
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 OLED.init(128, 64)
 ESP8266_IoT.connectWifi("Skywalker Main", "zulfi107")
 ESP8266_IoT.setMQTT(
@@ -10,7 +11,7 @@ ESP8266_IoT.SchemeList.TCP,
 "",
 ""
 )
-ESP8266_IoT.connectMQTT("10.0.0.3", 1883, true)
+ESP8266_IoT.connectMQTT("192.168.0.122", 1883, true)
 basic.pause(2000)
 basic.forever(function () {
     dht11_dht22.queryData(
@@ -27,8 +28,16 @@ basic.forever(function () {
     OLED.writeStringNewLine("DS Temp: " + convertToText(tempDS) + " C")
     OLED.writeStringNewLine("DHT Temp: " + convertToText(tempDHT) + " C")
     OLED.writeStringNewLine("DHT Hum: " + convertToText(humDHT) + " %")
+    if (ESP8266_IoT.wifiState(true)) {
+        OLED.writeStringNewLine("Wifi Connected")
+    } else {
+        OLED.writeStringNewLine("Wifi NOT Connected")
+    }
     if (ESP8266_IoT.isMqttBrokerConnected()) {
+        OLED.writeStringNewLine("MQTT Connected")
         ESP8266_IoT.publishMqttMessage("{\"temperatureDS\":" + convertToText(tempDS) + "," + "\"humidity\" : " + convertToText(humDHT) + "," + "\"temperatureDHT\": " + convertToText(tempDHT) + "}", "airsense/tempDHT", ESP8266_IoT.QosList.Qos0)
+    } else {
+        OLED.writeStringNewLine("MQTT NOT Connected")
     }
     basic.pause(5000)
 })
